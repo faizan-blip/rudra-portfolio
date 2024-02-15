a827fa4dab1ba519e9055e652ac6ba1caf1a6985
faizan-blip <faizanrock753@gmail.com>
2024-02-15 20:35:32 +0530

    config fixed

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qOZbajhl0rNJUSnIxlpJdawKNe339R2vVEJ3KsrTZaQ=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"LfKEe/XLPDQJRSOWQMnZMh20GcOW5xivix1fml5e6D4=\"}"
